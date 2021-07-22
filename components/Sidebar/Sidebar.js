63c66da6f7dfbf72f34f53efa0874422aa7d6973
Azura <azstima@gmail.com>
2021-07-22 12:09:24 +0700

    display data in admin

--- components/Sidebar/Sidebar.js
+++ components/Sidebar/Sidebar.js
@@ -26,7 +26,7 @@ export default function Sidebar() {
               href="#pablo"
               className="md:block text-left md:pb-2 text-blueGray-600 mr-0 inline-block whitespace-nowrap text-sm uppercase font-bold p-4 px-0"
             >
-              Notus NextJS
+              PMI Admin
             </a>
           </Link>
           {/* User */}
@@ -186,170 +186,170 @@ export default function Sidebar() {
               </li>
             </ul>
 
-            {/* Divider */}
-            <hr className="my-4 md:min-w-full" />
-            {/* Heading */}
-            <h6 className="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline">
-              Auth Layout Pages
-            </h6>
-            {/* Navigation */}
+            {/*/!* Divider *!/*/}
+            {/*<hr className="my-4 md:min-w-full" />*/}
+            {/*/!* Heading *!/*/}
+            {/*<h6 className="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline">*/}
+            {/*  Auth Layout Pages*/}
+            {/*</h6>*/}
+            {/*/!* Navigation *!/*/}
 
-            <ul className="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">
-              <li className="items-center">
-                <Link href="/auth/login">
-                  <a
-                    href="#pablo"
-                    className="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"
-                  >
-                    <i className="fas fa-fingerprint text-blueGray-400 mr-2 text-sm"></i>{" "}
-                    Login
-                  </a>
-                </Link>
-              </li>
+            {/*<ul className="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">*/}
+            {/*  <li className="items-center">*/}
+            {/*    <Link href="/auth/login">*/}
+            {/*      <a*/}
+            {/*        href="#pablo"*/}
+            {/*        className="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"*/}
+            {/*      >*/}
+            {/*        <i className="fas fa-fingerprint text-blueGray-400 mr-2 text-sm"></i>{" "}*/}
+            {/*        Login*/}
+            {/*      </a>*/}
+            {/*    </Link>*/}
+            {/*  </li>*/}
 
-              <li className="items-center">
-                <Link href="/auth/register">
-                  <a
-                    href="#pablo"
-                    className="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"
-                  >
-                    <i className="fas fa-clipboard-list text-blueGray-300 mr-2 text-sm"></i>{" "}
-                    Register
-                  </a>
-                </Link>
-              </li>
-            </ul>
-
-            {/* Divider */}
-            <hr className="my-4 md:min-w-full" />
-            {/* Heading */}
-            <h6 className="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline">
-              No Layout Pages
-            </h6>
-            {/* Navigation */}
-
-            <ul className="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">
-              <li className="items-center">
-                <Link href="/landing">
-                  <a
-                    href="#pablo"
-                    className="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"
-                  >
-                    <i className="fas fa-newspaper text-blueGray-400 mr-2 text-sm"></i>{" "}
-                    Landing Page
-                  </a>
-                </Link>
-              </li>
-
-              <li className="items-center">
-                <Link href="/profile">
-                  <a
-                    href="#pablo"
-                    className="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"
-                  >
-                    <i className="fas fa-user-circle text-blueGray-400 mr-2 text-sm"></i>{" "}
-                    Profile Page
-                  </a>
-                </Link>
-              </li>
-            </ul>
+            {/*  <li className="items-center">*/}
+            {/*    <Link href="/auth/register">*/}
+            {/*      <a*/}
+            {/*        href="#pablo"*/}
+            {/*        className="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"*/}
+            {/*      >*/}
+            {/*        <i className="fas fa-clipboard-list text-blueGray-300 mr-2 text-sm"></i>{" "}*/}
+            {/*        Register*/}
+            {/*      </a>*/}
+            {/*    </Link>*/}
+            {/*  </li>*/}
+            {/*</ul>*/}
 
             {/* Divider */}
-            <hr className="my-4 md:min-w-full" />
+            {/*<hr className="my-4 md:min-w-full" />*/}
             {/* Heading */}
-            <h6 className="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline">
-              Documentation
-            </h6>
-            {/* Navigation */}
-            <ul className="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">
-              <li className="inline-flex">
-                <a
-                  href="https://www.creative-tim.com/learning-lab/tailwind/nextjs/colors/notus"
-                  target="_blank"
-                  className="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
-                >
-                  <i className="fas fa-paint-brush mr-2 text-blueGray-300 text-base"></i>
-                  Styles
-                </a>
-              </li>
+            {/*<h6 className="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline">*/}
+            {/*  No Layout Pages*/}
+            {/*</h6>*/}
+            {/*/!* Navigation *!/*/}
 
-              <li className="inline-flex">
-                <a
-                  href="https://www.creative-tim.com/learning-lab/tailwind/nextjs/alerts/notus"
-                  target="_blank"
-                  className="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
-                >
-                  <i className="fab fa-css3-alt mr-2 text-blueGray-300 text-base"></i>
-                  CSS Components
-                </a>
-              </li>
+            {/*<ul className="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">*/}
+            {/*  <li className="items-center">*/}
+            {/*    <Link href="/landing">*/}
+            {/*      <a*/}
+            {/*        href="#pablo"*/}
+            {/*        className="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"*/}
+            {/*      >*/}
+            {/*        <i className="fas fa-newspaper text-blueGray-400 mr-2 text-sm"></i>{" "}*/}
+            {/*        Landing Page*/}
+            {/*      </a>*/}
+            {/*    </Link>*/}
+            {/*  </li>*/}
 
-              <li className="inline-flex">
-                <a
-                  href="https://www.creative-tim.com/learning-lab/tailwind/angular/overview/notus"
-                  target="_blank"
-                  className="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
-                >
-                  <i className="fab fa-angular mr-2 text-blueGray-300 text-base"></i>
-                  Angular
-                </a>
-              </li>
+            {/*  <li className="items-center">*/}
+            {/*    <Link href="/profile">*/}
+            {/*      <a*/}
+            {/*        href="#pablo"*/}
+            {/*        className="text-blueGray-700 hover:text-blueGray-500 text-xs uppercase py-3 font-bold block"*/}
+            {/*      >*/}
+            {/*        <i className="fas fa-user-circle text-blueGray-400 mr-2 text-sm"></i>{" "}*/}
+            {/*        Profile Page*/}
+            {/*      </a>*/}
+            {/*    </Link>*/}
+            {/*  </li>*/}
+            {/*</ul>*/}
 
-              <li className="inline-flex">
-                <a
-                  href="https://www.creative-tim.com/learning-lab/tailwind/js/overview/notus"
-                  target="_blank"
-                  className="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
-                >
-                  <i className="fab fa-js-square mr-2 text-blueGray-300 text-base"></i>
-                  Javascript
-                </a>
-              </li>
+            {/* Divider */}
+            {/*<hr className="my-4 md:min-w-full" />*/}
+            {/*/!* Heading *!/*/}
+            {/*<h6 className="md:min-w-full text-blueGray-500 text-xs uppercase font-bold block pt-1 pb-4 no-underline">*/}
+            {/*  Documentation*/}
+            {/*</h6>*/}
+            {/*/!* Navigation *!/*/}
+            {/*<ul className="md:flex-col md:min-w-full flex flex-col list-none md:mb-4">*/}
+            {/*  <li className="inline-flex">*/}
+            {/*    <a*/}
+            {/*      href="https://www.creative-tim.com/learning-lab/tailwind/nextjs/colors/notus"*/}
+            {/*      target="_blank"*/}
+            {/*      className="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"*/}
+            {/*    >*/}
+            {/*      <i className="fas fa-paint-brush mr-2 text-blueGray-300 text-base"></i>*/}
+            {/*      Styles*/}
+            {/*    </a>*/}
+            {/*  </li>*/}
 
-              <li className="inline-flex">
-                <a
-                  href="https://www.creative-tim.com/learning-lab/tailwind/nextjs/overview/notus"
-                  target="_blank"
-                  className="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
-                >
-                  <i className="fab fa-react mr-2 text-blueGray-300 text-base"></i>
-                  NextJS
-                </a>
-              </li>
+            {/*  <li className="inline-flex">*/}
+            {/*    <a*/}
+            {/*      href="https://www.creative-tim.com/learning-lab/tailwind/nextjs/alerts/notus"*/}
+            {/*      target="_blank"*/}
+            {/*      className="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"*/}
+            {/*    >*/}
+            {/*      <i className="fab fa-css3-alt mr-2 text-blueGray-300 text-base"></i>*/}
+            {/*      CSS Components*/}
+            {/*    </a>*/}
+            {/*  </li>*/}
 
-              <li className="inline-flex">
-                <a
-                  href="https://www.creative-tim.com/learning-lab/tailwind/react/overview/notus"
-                  target="_blank"
-                  className="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
-                >
-                  <i className="fab fa-react mr-2 text-blueGray-300 text-base"></i>
-                  React
-                </a>
-              </li>
+            {/*  <li className="inline-flex">*/}
+            {/*    <a*/}
+            {/*      href="https://www.creative-tim.com/learning-lab/tailwind/angular/overview/notus"*/}
+            {/*      target="_blank"*/}
+            {/*      className="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"*/}
+            {/*    >*/}
+            {/*      <i className="fab fa-angular mr-2 text-blueGray-300 text-base"></i>*/}
+            {/*      Angular*/}
+            {/*    </a>*/}
+            {/*  </li>*/}
 
-              <li className="inline-flex">
-                <a
-                  href="https://www.creative-tim.com/learning-lab/tailwind/svelte/overview/notus"
-                  target="_blank"
-                  className="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
-                >
-                  <i className="fas fa-link mr-2 text-blueGray-300 text-base"></i>
-                  Svelte
-                </a>
-              </li>
+            {/*  <li className="inline-flex">*/}
+            {/*    <a*/}
+            {/*      href="https://www.creative-tim.com/learning-lab/tailwind/js/overview/notus"*/}
+            {/*      target="_blank"*/}
+            {/*      className="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"*/}
+            {/*    >*/}
+            {/*      <i className="fab fa-js-square mr-2 text-blueGray-300 text-base"></i>*/}
+            {/*      Javascript*/}
+            {/*    </a>*/}
+            {/*  </li>*/}
 
-              <li className="inline-flex">
-                <a
-                  href="https://www.creative-tim.com/learning-lab/tailwind/vue/overview/notus"
-                  target="_blank"
-                  className="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"
-                >
-                  <i className="fab fa-vuejs mr-2 text-blueGray-300 text-base"></i>
-                  VueJS
-                </a>
-              </li>
-            </ul>
+            {/*  <li className="inline-flex">*/}
+            {/*    <a*/}
+            {/*      href="https://www.creative-tim.com/learning-lab/tailwind/nextjs/overview/notus"*/}
+            {/*      target="_blank"*/}
+            {/*      className="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"*/}
+            {/*    >*/}
+            {/*      <i className="fab fa-react mr-2 text-blueGray-300 text-base"></i>*/}
+            {/*      NextJS*/}
+            {/*    </a>*/}
+            {/*  </li>*/}
+
+            {/*  <li className="inline-flex">*/}
+            {/*    <a*/}
+            {/*      href="https://www.creative-tim.com/learning-lab/tailwind/react/overview/notus"*/}
+            {/*      target="_blank"*/}
+            {/*      className="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"*/}
+            {/*    >*/}
+            {/*      <i className="fab fa-react mr-2 text-blueGray-300 text-base"></i>*/}
+            {/*      React*/}
+            {/*    </a>*/}
+            {/*  </li>*/}
+
+            {/*  <li className="inline-flex">*/}
+            {/*    <a*/}
+            {/*      href="https://www.creative-tim.com/learning-lab/tailwind/svelte/overview/notus"*/}
+            {/*      target="_blank"*/}
+            {/*      className="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"*/}
+            {/*    >*/}
+            {/*      <i className="fas fa-link mr-2 text-blueGray-300 text-base"></i>*/}
+            {/*      Svelte*/}
+            {/*    </a>*/}
+            {/*  </li>*/}
+
+            {/*  <li className="inline-flex">*/}
+            {/*    <a*/}
+            {/*      href="https://www.creative-tim.com/learning-lab/tailwind/vue/overview/notus"*/}
+            {/*      target="_blank"*/}
+            {/*      className="text-blueGray-700 hover:text-blueGray-500 text-sm block mb-4 no-underline font-semibold"*/}
+            {/*    >*/}
+            {/*      <i className="fab fa-vuejs mr-2 text-blueGray-300 text-base"></i>*/}
+            {/*      VueJS*/}
+            {/*    </a>*/}
+            {/*  </li>*/}
+            {/*</ul>*/}
           </div>
         </div>
       </nav>
--- pages/admin/dashboard.js
+++ pages/admin/dashboard.js
@@ -1,4 +1,7 @@
 import React from "react";
+import PropTypes from "prop-types";
+import client from "../apollo-client";
+import {gql} from "@apollo/client";
 
 // components
 
@@ -10,28 +13,108 @@ import CardSocialTraffic from "components/Cards/CardSocialTraffic.js";
 // layout for page
 
 import Admin from "layouts/Admin.js";
+import CardTable from "../../components/Cards/CardTable";
 
-export default function Dashboard() {
-  return (
-    <>
-      <div className="flex flex-wrap">
-        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-          <CardLineChart />
-        </div>
-        <div className="w-full xl:w-4/12 px-4">
-          <CardBarChart />
-        </div>
-      </div>
-      <div className="flex flex-wrap mt-4">
-        <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">
-          <CardPageVisits />
-        </div>
-        <div className="w-full xl:w-4/12 px-4">
-          <CardSocialTraffic />
-        </div>
-      </div>
-    </>
-  );
+
+export default function Dashboard({udds, color}) {
+      return (
+        <>
+            <div
+                className={
+                    "relative flex flex-col min-w-0 break-words w-full mb-6 shadow-lg rounded " +
+                    (color === "light" ? "bg-white" : "bg-blueGray-700 text-white")
+                }
+            >
+            <div className="rounded-t mb-0 px-4 py-3 border-0">
+                <div className="flex flex-wrap items-center">
+                    <div className="relative w-full px-4 max-w-full flex-grow flex-1">
+                        <h3
+                            className={
+                                "font-semibold text-lg " +
+                                (color === "light" ? "text-blueGray-700" : "text-white")
+                            }
+                        >
+                            Daftar PMI
+                        </h3>
+                    </div>
+                </div>
+            </div>
+            <div className="block w-full overflow-x-auto">
+            <table className="items-center w-full bg-transparent border-collapse">
+                <thead>
+                <tr>
+                    <th
+                        className={
+                            "px-6 align-middle border border-solid py-3 text-xs uppercase border-l-0 border-r-0 whitespace-nowrap font-semibold text-left " +
+                            (color === "light"
+                                ? "bg-blueGray-50 text-blueGray-500 border-blueGray-100"
+                                : "bg-blueGray-600 text-blueGray-200 border-blueGray-500")
+                        }
+                    >
+                        Nama PMI
+                    </th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    udds.map((udd) => {
+                        const { udd_name: uddName } = udd
+                        return <tr className="border-t-0 px-6 align-middle border-l-0 border-r-0 text-xs whitespace-nowrap p-4 text-left flex items-center">
+                            <td>
+                                {uddName}
+                            </td>
+                        </tr>
+                    })
+                }
+                </tbody>
+            </table>
+            </div>
+            </div>
+          {/*<div className="flex flex-wrap">*/}
+          {/*  <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">*/}
+          {/*    <CardLineChart />*/}
+          {/*  </div>*/}
+          {/*  <div className="w-full xl:w-4/12 px-4">*/}
+          {/*    <CardBarChart />*/}
+          {/*  </div>*/}
+          {/*</div>*/}
+          {/*<div className="flex flex-wrap mt-4">*/}
+          {/*  <div className="w-full xl:w-8/12 mb-12 xl:mb-0 px-4">*/}
+          {/*    <CardPageVisits />*/}
+          {/*  </div>*/}
+          {/*  <div className="w-full xl:w-4/12 px-4">*/}
+          {/*    <CardSocialTraffic />*/}
+          {/*  </div>*/}
+          {/*</div>*/}
+        </>
+      );
 }
 
 Dashboard.layout = Admin;
+
+export async function getStaticProps() {
+    const { data } = await client.query({
+        query: gql`
+        query getAllUddsQuery {
+          getAllUdds {
+            id
+            udd_name
+          }
+        }
+      `,
+    });
+
+    return {
+        props: {
+            udds: data.getAllUdds,
+        },
+    };
+}
+
+CardTable.defaultProps = {
+    color: "light",
+};
+
+CardTable.propTypes = {
+    color: PropTypes.oneOf(["light", "dark"]),
+};
